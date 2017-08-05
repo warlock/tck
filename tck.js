ed5665385e55bfe3adf37e3293fcad3f0f0f1801
Josep Subils Rigau <josep@spellbook.io>
2017-08-05 10:08:27 +0200

    [documentation] add jsdocs in comments

--- tck.js
+++ tck.js
@@ -1,37 +1,92 @@
 var tck = {
-  'isFunction': function (fn) {
-    return typeof fn === 'function';
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is a function
+   */
+  'isFunction': function (data) {
+    return typeof data === 'function';
   },
-  'isArray': function (obj) {
-    return typeof obj === "object" && obj instanceof Array;
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is a array
+   */
+  'isArray': function (data) {
+    return typeof data === "object" && data instanceof Array;
   },
-  'isObject': function (obj) {
-    return (typeof obj === "object") && !(obj instanceof Array) && obj !== null;
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is a object
+   */
+  'isObject': function (data) {
+    return (typeof data === "object") && !(data instanceof Array) && data !== null;
   },
-  'isNumber': function (obj) {
-    return typeof obj === "number" || obj instanceof Number;
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is a number
+   */
+  'isNumber': function (data) {
+    return typeof data === "number" || data instanceof Number;
   },
-  'isInteger': function (obj) {
-    if (tck.isNumber(obj)) return obj % 1 === 0;
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is a integer
+   */
+  'isInteger': function (data) {
+    if (tck.isNumber(data)) return data % 1 === 0;
     else return false;
   },
-  'isString': function (obj ) {
-    return typeof obj === "string" || obj instanceof String;
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is a string
+   */
+  'isString': function (data) {
+    return typeof data === "string" || data instanceof String;
   },
-  'isBoolean': function (obj) {
-    return typeof obj === "boolean";
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is boolean
+   */
+  'isBoolean': function (data) {
+    return typeof data === "boolean";
   },
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Returns true when data is empty
+   */
   'isEmpty': function (data) {
-    return (undefined === data || data === null || data === "");
+    return tck.isUndefined(data) || tck.isNull(data) || data === "";
   },
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is null
+   */
   'isNull': function (data) {
     return data === null;
   },
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is not a number
+   */
   'isNaN': function (data) {
     return isNaN(data);
   },
+
+  /**
+   * @param {Any} data : Data for evaluation.
+   * @returns {Boolean} : Validate data is undefined
+   */
   'isUndefined': function (data) {
-    return data === undefined;
+    return undefined === data;
   }
 };
 
