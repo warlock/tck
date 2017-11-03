a258d0a84da57a7aac5b1dd2d9ee5893a9b6a019
Josep Subils <js@js.gl>
2017-11-03 09:54:12 +0100

    [add] get function and alias

--- tck.js
+++ tck.js
@@ -4,7 +4,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is a function
    */
-  'isFunction': function (data) {
+  isFunction: function (data) {
     return typeof data === 'function';
   },
 
@@ -12,7 +12,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is a array
    */
-  'isArray': function (data) {
+  isArray: function (data) {
     return typeof data === "object" && data instanceof Array;
   },
 
@@ -20,7 +20,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is a object
    */
-  'isObject': function (data) {
+  isObject: function (data) {
     return (typeof data === "object") && !(data instanceof Array) && data !== null;
   },
 
@@ -28,7 +28,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is a number
    */
-  'isNumber': function (data) {
+  isNumber: function (data) {
     return typeof data === "number" || data instanceof Number;
   },
 
@@ -36,7 +36,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is a integer
    */
-  'isInteger': function (data) {
+  isInteger: function (data) {
     if (tck.isNumber(data)) return data % 1 === 0;
     else return false;
   },
@@ -45,7 +45,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is a string
    */
-  'isString': function (data) {
+  isString: function (data) {
     return typeof data === "string" || data instanceof String;
   },
 
@@ -53,7 +53,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is boolean
    */
-  'isBoolean': function (data) {
+  isBoolean: function (data) {
     return typeof data === "boolean";
   },
 
@@ -61,7 +61,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Returns true when data is empty
    */
-  'isEmpty': function (data) {
+  isEmpty: function (data) {
     return tck.isUndefined(data) || tck.isNull(data) || data === "";
   },
 
@@ -69,7 +69,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is null
    */
-  'isNull': function (data) {
+  isNull: function (data) {
     return data === null;
   },
 
@@ -77,7 +77,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is not a number
    */
-  'isNaN': function (data) {
+  isNaN: function (data) {
     return isNaN(data);
   },
 
@@ -85,7 +85,7 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Validate data is undefined
    */
-  'isUndefined': function (data) {
+  isUndefined: function (data) {
     return undefined === data;
   },
 
@@ -93,9 +93,43 @@ var tck = {
    * @param {Any} data : Data for evaluation.
    * @returns {Boolean} : Returns true when data is set
    */
-  'isSet': function (data) {
+  isSet: function (data) {
     return !tck.isEmpty(data);
+  },
+
+  /**
+   *  @param {Any} obj : Data for evaluation
+   * @param {String} route : Route from data
+   *  @returns {Any} : Returns element in object
+   */
+  get: function (obj, route) {
+    if (tck.isSet(obj) && tck.isSet(route)) {
+      route = route.split(".");
+      if (route.length === 1 ) return obj[route[0]];
+      else {
+        for (var i = 0; i < route.length; i++) {
+          if (tck.isSet(obj[route[i]])) obj = obj[route[i]];
+          else return undefined;
+        }
+        return obj;
+      }
+    } else return undefined;
   }
 };
+
+// Function alias
+tck.f = tck.isFunction;
+tck.a = tck.isArray;
+tck.o = tck.isObject;
+tck.n = tck.isNumber;
+tck.i = tck.isInteger;
+tck.s = tck.isString;
+tck.b = tck.isBoolean;
+tck.e = tck.isEmpty;
+tck.null = tck.isNull;
+tck.nan = tck.isNaN;
+tck.u = tck.isUndefined;
+tck.set = tck.isSet;
+tck.g = tck.get;
 
 module.exports = tck;
